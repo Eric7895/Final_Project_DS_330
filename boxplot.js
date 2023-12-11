b74f1b452973f916c658d355ed406958953f18ea
eastkite00 <danny379k@gmail.com>
2023-12-10 23:27:39 -0500

    .
    .

--- boxplot.js
+++ boxplot.js
@@ -1,129 +1,103 @@
-// Load the data
-d3.csv("Sleep.csv", function (error, data) {
-    if (error) {
-        console.error("Error loading data:", error);
-        return;
-    }
+// Function to draw the boxplot for a given variable
+function drawBoxplot(selectedVariable) {
+    // Clear any previous svg content
+    const svg = d3.select(".boxplot");
+    svg.selectAll("*").remove();
 
-    // Set the dimensions and margins of the graph
-    var margin = { top: 10, right: 30, bottom: 30, left: 40 },
-        width = 460 - margin.left - margin.right,
-        height = 400 - margin.top - margin.bottom;
+    const margin = { top: 10, right: 30, bottom: 30, left: 40 },
+          width = +svg.attr("width") - margin.left - margin.right,
+          height = +svg.attr("height") - margin.top - margin.bottom;
 
-    // Append the svg object to the body of the page
-    var svg = d3.select(".boxplot")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    const g = svg.append("g").attr("transform", `translate(${margin.left},${margin.top})`);
 
-    // Create a function to prepare the data for boxplot
-    function prepareBoxplotData(data, variable) {
-        var boxplotData = d3.nest()
-            .key(function (d) { return d[variable]; })
-            .rollup(function (d) {
-                var values = d.map(function (item) {
-                    return +item[variable]; // Convert to numeric value
-                });
+    // Load the data
+    d3.csv("Sleep.csv").then(function(data) {
+        // Convert all data to numerical values
+        data.forEach(function(d) {
+            d[selectedVariable] = +d[selectedVariable];
+        });
 
-                var sortedValues = values.slice().sort(d3.ascending);
+        // Filter non-numeric values as they cannot be used in a boxplot
+        data = data.filter(function(d) { return !isNaN(d[selectedVariable]); });
 
-                var q1 = d3.quantile(sortedValues, 0.25);
-                var median = d3.quantile(sortedValues, 0.5);
-                var q3 = d3.quantile(sortedValues, 0.75);
+        const values = data.map(d => d[selectedVariable]).sort(d3.ascending);
+        const q1 = d3.quantile(values, .25);
+        const median = d3.quantile(values, .5);
+        const q3 = d3.quantile(values, .75);
+        const interQuantileRange = q3 - q1;
+        const lowerWhisker = Math.max(d3.min(values), q1 - 1.5 * interQuantileRange);
+        const upperWhisker = Math.min(d3.max(values), q3 + 1.5 * interQuantileRange);
 
-                var interQuantileRange = q3 - q1;
-                var min = q1 - 1.5 * interQuantileRange;
-                var max = q3 + 1.5 * interQuantileRange;
+        const x = d3.scaleLinear()
+            .domain([d3.min(values), d3.max(values)])
+            .range([0, width]);
 
-                // Filter outliers within the range
-                var outliers = values.filter(function (d) { return d < min || d > max; });
+        const y = d3.scaleBand()
+            .range([height, 0])
+            .domain([selectedVariable])
+            .padding(0.2);
 
-                return {
-                    outliers: outliers,
-                    q1: q1,
-                    median: median,
-                    q3: q3,
-                    min: min,
-                    max: max
-                };
-            })
-            .entries(data);
+        // Box
+        g.append("rect")
+            .attr("x", x(q1))
+            .attr("y", y(selectedVariable))
+            .attr("width", x(q3) - x(q1))
+            .attr("height", y.bandwidth())
+            .attr("fill", "#69b3a2");
 
-        return boxplotData;
-    }
+        // Median line
+        g.append("line")
+            .attr("x1", x(median))
+            .attr("x2", x(median))
+            .attr("y1", y(selectedVariable))
+            .attr("y2", y(selectedVariable) + y.bandwidth())
+            .attr("stroke", "black");
 
-    // Choose the variable you want to visualize
-    var selectedVariable = "age"; // Change this to the desired variable
+        // Whiskers
+        g.append("line")
+            .attr("x1", x(lowerWhisker))
+            .attr("x2", x(upperWhisker))
+            .attr("y1", y(selectedVariable) + y.bandwidth() / 2)
+            .attr("y2", y(selectedVariable) + y.bandwidth() / 2)
+            .attr("stroke", "black");
 
-    // Prepare the data for the selected variable
-    var boxplotData = prepareBoxplotData(data, selectedVariable);
+        // Whisker caps
+        g.append("line")
+            .attr("x1", x(lowerWhisker))
+            .attr("x2", x(lowerWhisker))
+            .attr("y1", y(selectedVariable))
+            .attr("y2", y(selectedVariable) + y.bandwidth())
+            .attr("stroke", "black");
 
-    console.log("Boxplot Data:", boxplotData);
+        g.append("line")
+            .attr("x1", x(upperWhisker))
+            .attr("x2", x(upperWhisker))
+            .attr("y1", y(selectedVariable))
+            .attr("y2", y(selectedVariable) + y.bandwidth())
+            .attr("stroke", "black");
 
-    // Build and show the Y scale
-    var y = d3.scaleBand()
-        .domain(boxplotData.map(function (d) { return d.key; }))
-        .range([height, 0])
-        .padding(0.1);
+        // Outliers
+        const outliers = data.filter(d => d[selectedVariable] < lowerWhisker || d[selectedVariable] > upperWhisker);
 
-    console.log("Y Domain:", y.domain());
+        g.selectAll(".outlier")
+            .data(outliers)
+            .enter().append("circle")
+            .attr("class", "outlier")
+            .attr("cx", function(d) { return x(d[selectedVariable]); })
+            .attr("cy", y(selectedVariable) + y.bandwidth() / 2)
+            .attr("r", 3)
+            .attr("fill", "red");
+    });
+}
 
-    // Build and show the X scale
-    var x = d3.scaleLinear()
-        .domain([d3.min(data, function (d) { return +d[selectedVariable]; }), d3.max(data, function (d) { return +d[selectedVariable]; })])
-        .range([0, width]);
+// Initial call to draw the boxplot for the default selected variable
+drawBoxplot("Age"); // Replace "Age" with your default variable
 
-    console.log("X Domain:", x.domain());
-
-    // Build the boxplots
-    svg.selectAll(".box")
-        .data(boxplotData)
-        .enter().append("g")
-        .attr("transform", function (d) { return "translate(0," + y(d.key) + ")"; })
-        .append("rect")
-        .attr("x", function (d) { return x(d.value.min); })
-        .attr("y", y.bandwidth() / 4)
-        .attr("width", function (d) { return x(d.value.max) - x(d.value.min); })
-        .attr("height", y.bandwidth() / 2)
-        .style("fill", "#69b3a2")
-        .attr("stroke", "black");
-
-    // Add a dot for each outlier
-    svg.selectAll(".outlier")
-        .data(boxplotData)
-        .enter().append("g")
-        .attr("transform", function (d) { return "translate(0," + y(d.key) + ")"; })
-        .selectAll("circle")
-        .data(function (d) { return d.value.outliers; })
-        .enter().append("circle")
-        .attr("cx", function (d) { return x(d); })
-        .attr("cy", y.bandwidth() / 2)
-        .attr("r", 3)
-        .style("fill", "red");
-
-    // Add a line for the median
-    svg.selectAll(".median")
-        .data(boxplotData)
-        .enter().append("g")
-        .attr("transform", function (d) { return "translate(0," + y(d.key) + ")"; })
-        .append("line")
-        .attr("x1", function (d) { return x(d.value.median); })
-        .attr("x2", function (d) { return x(d.value.median); })
-        .attr("y1", y.bandwidth() / 4)
-        .attr("y2", y.bandwidth() / 1.33)
-        .style("stroke", "black");
-
-    // Add a box for the main box
-    svg.selectAll(".box")
-        .data(boxplotData)
-        .enter().append("g")
-        .attr("transform", function (d) { return "translate(0," + y(d.key) + ")"; })
-        .append("rect")
-        .attr("x", function (d) { return x(d.value.q1); })
-        .attr("y", y.bandwidth() / 4)
-        .attr("width", function (d) { return x(d.value.q3) - x(d.value.q1); })
-        .attr("height", y.bandwidth() / 2)
-        .style("fill", "#69b3a2")
-        .attr("stroke", "black");
+// Event listener for the dropdown change
+d3.select("#boxplotVariable").on("change", function() {
+    // Get the newly selected variable
+    var newVariable = d3.select(this).property("value");
+    
+    // Redraw the boxplot with the new variable
+    drawBoxplot(newVariable);
 });
